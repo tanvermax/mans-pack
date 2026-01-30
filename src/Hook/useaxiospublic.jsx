f14a4819169e8d6a359a49ea9a472b891b0b53ab
tanver909 <tanveermahidi@gmial.com>
2026-01-30 10:01:05 +0600

    edit dunction added

--- src/Hook/useaxiospublic.jsx
+++ src/Hook/useaxiospublic.jsx
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const axiosPublic = axios.create({
     // baseURL: "https://mans-server.vercel.app",
-    // baseURL: "https://mans-server.vercel.app",
+    baseURL: "https://mans-server.vercel.app",
     
-    baseURL: "http://localhost:5000",
+    // baseURL: "http://localhost:5000",
     
   });
 const useaxiospublic = () => {
